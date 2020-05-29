8ec2680eb1d5aab76cb240702151a8317ad19ca5
Travis Roberts <27257343+tsrob50@users.noreply.github.com>
2020-05-29 15:32:23 -0500

    Test file for Linux installer

---
+++ installer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# installer script
+# simple but works
+
+date >>installoutput.txt
+sudo yum -y install fontconfig >>installoutput.txt
+sudo yum -y install freetype >>installoutput.txt
+sudo yum -y install gettext >>installoutput.txt
+sudo yum -y install grub2 >>installoutput.txt
+sudo yum -y install libjpeg-turbo >>installoutput.txt
+sudo yum -y install libpng >>installoutput.txt
+sudo yum -y install libXext >>installoutput.txt
+sudo yum -y install libXrender >>installoutput.txt
+sudo yum -y install libxslt >>installoutput.txt
+sudo yum -y install apr >>installoutput.txt
+sudo yum -y install apr >>installoutput.txt
+sudo yum -y install glibc >>installoutput.txt
+sudo yum -y install libevent >>installoutput.txt
+sudo yum -y install libselinux >>installoutput.txt
+sudo yum -y install mailcap >>installoutput.txt
+sudo yum -y install openssl-libs >>installoutput.txt
+sudo yum -y install systemd >>installoutput.txt
+sudo yum -y install yajl >>installoutput.txt
