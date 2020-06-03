d219f410437bc93d7abbe2a799099d05f7e6b4b1
Travis Roberts <27257343+tsrob50@users.noreply.github.com>
2020-06-03 09:48:20 -0500

    Add Drive configuration.  Fix log path

--- installer.sh
+++ installer.sh
@@ -4,7 +4,7 @@
 # simple but works
 
 date >>/var/log/installoutput.txt
-sudo yum -y install fontconfig >>/var/log//var/log/installoutput.txt
+sudo yum -y install fontconfig >>/var/log/installoutput.txt
 sudo yum -y install freetype >>/var/log/installoutput.txt
 sudo yum -y install gettext >>/var/log/installoutput.txt
 sudo yum -y install grub2 >>/var/log/installoutput.txt
@@ -23,11 +23,19 @@ sudo yum -y install openssl-libs >>/var/log/installoutput.txt
 sudo yum -y install systemd >>/var/log/installoutput.txt
 sudo yum -y install yajl >>/var/log/installoutput.txt
 
-# Need to add the host file update
-# sudo echo "192.241.xx.xx  venus.example.com venus" >> /etc/hosts
+# Need to add the host file update for Informatica servers
 sudo echo "192.168.254.1 testentry.example.com testentry" >> /etc/hosts
 
 # Add ULimit https://access.redhat.com/solutions/61334
 sudo echo "*    soft    nofile  32" >>/etc/security/limits.conf 
 sudo echo "*    hard    nofile  64" >>/etc/security/limits.conf
 sudo echo "*    hard    nproc  4096" >>/etc/security/limits.conf 
+
+
+# Format Hard Drive
+# https://serverfault.com/questions/258152/fdisk-partition-in-single-line
+sudo sfdisk -l >>/var/log/installoutput.txt
+dev='/dev/sdc' >>/var/log/installoutput.txt
+sudo umount "$dev" >>/var/log/installoutput.txt
+printf "o\nn\np\n1\n\n\nw\n" | sudo fdisk "$dev" >>/var/log/installoutput.txt
+sudo mkfs.ext4 "${dev}1" >>/var/log/installoutput.txt
